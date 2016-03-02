9951481ef792cb1bcfc42ac7a30c2fee1e550425
Laura <lauracleave29@gmail.com>
2016-03-02 19:20:05 +1100

    15min-Challenge-Commit

---
+++ GA-FEWD10-L14-EX1/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('h1').click(function () {
+        $(this).css('font-size', '50px')
+    });
+    $('p').click(function () {
+        $(this).css('color', 'blue');
+        $(this).css('font-size', '28px')
+    });
+
+});
